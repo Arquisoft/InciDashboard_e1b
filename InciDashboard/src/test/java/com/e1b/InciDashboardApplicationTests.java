098ecfb1c50816aeab0978421a46bf8ed760829a
mariodod <mario.dod@gmail.com>
2018-03-06 10:42:14 +0100

    test

--- InciDashboard/src/test/java/com/e1b/InciDashboardApplicationTests.java
+++ InciDashboard/src/test/java/com/e1b/InciDashboardApplicationTests.java
@@ -1,5 +1,7 @@
 package com.e1b;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,7 @@ public class InciDashboardApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertTrue(true);
 	}
 
 }
