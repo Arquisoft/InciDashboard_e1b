5e1ff965753e4ca154ca71a6834085827a18b532
mariodod <mario.dod@gmail.com>
2018-03-06 09:48:51 +0100

    Esqueleto de inciDashboard

---
+++ InciDashboard/src/main/java/com/e1b/InciDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.e1b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InciDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InciDashboardApplication.class, args);
+	}
+}
