698695cd95ad799d8fc79155ace95a35cff728b3
mariodod <mario.dod@gmail.com>
2018-03-14 18:24:47 +0100

    arreglado arranque

---
+++ src/main/java/com/e1b/CustomConfiguration.java
@@ -0,0 +1,30 @@
+package com.e1b;
+import java.util.List;
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class CustomConfiguration extends  WebMvcConfigurerAdapter{
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+
+		PageableHandlerMethodArgumentResolver resolver =
+				new PageableHandlerMethodArgumentResolver();
+		
+		resolver.setFallbackPageable(new PageRequest(0, 5));
+		argumentResolvers.add(resolver);
+		super.addArgumentResolvers(argumentResolvers);
+	}
+	
+}
