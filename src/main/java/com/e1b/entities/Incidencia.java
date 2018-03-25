8b2b1ce66a9ca35873d9119d2acdea1ac6a4801a
mariodod <mario.dod@gmail.com>
2018-03-25 19:11:00 +0200

    Carga de marcadores de las incidencias usando thymeleaf

--- src/main/java/com/e1b/entities/Incidencia.java
+++ src/main/java/com/e1b/entities/Incidencia.java
@@ -3,8 +3,17 @@ package com.e1b.entities;
 public class Incidencia {
 	
 	private Operario operario;
+	private String name;
+	private double lat;
+	private double lng;
 	
 	public Incidencia() {}
+
+	public Incidencia(String name, double lat, double lng) {
+		this.setName(name);
+		this.setLat(lat);
+		this.setLng(lng);
+	}
 
 	public Operario getOperario() {
 		return operario;
@@ -12,6 +21,30 @@ public class Incidencia {
 
 	public void setOperario(Operario operario) {
 		this.operario = operario;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public double getLng() {
+		return lng;
+	}
+
+	public void setLng(double lng) {
+		this.lng = lng;
 	}
 
 }
--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,33 +1,20 @@
 var mapa;
-var miMarcador01 = new google.maps.LatLng(43.3661,  -5.8425);
-var miMarcador02 = new google.maps.LatLng(43.3639,  -5.8420);
-var miMarcador03 = new google.maps.LatLng(43.3620,  -5.8412);
-    
+	        
 function initialize() {
-  var mapaOpciones = {
-    zoom: 15,
-    center: miMarcador01
-  };
-  mapa = new google.maps.Map(document.getElementById('mapa-canvas'), mapaOpciones);
-
-var marcador01 = new google.maps.Marker({
-      position: miMarcador01,
-      map: mapa,
-      title: 'Marcador 01'
-  });
-
-var marcador02 = new google.maps.Marker({
-      position: miMarcador02,
-      map: mapa,
-      title: 'Marcador 02'
-  });
-    
-var marcador03 = new google.maps.Marker({
-      position: miMarcador03,
-      map: mapa,
-      title: 'Marcador 03'
-  });
-
+	var oviedo = new google.maps.LatLng(43.3500,  -5.8500);
+	var mapaOpciones = {
+	   zoom: 15,
+	   center: oviedo,
+	 };
+	 mapa = new google.maps.Map(document.getElementById('mapa-canvas'), mapaOpciones);
 }
-
+	
+function addMarcador(localizacion,name) {
+	var marcador = new google.maps.Marker({
+	  position: localizacion,
+	  map: mapa
+	  title: name
+	});
+}
+	   
 google.maps.event.addDomListener(window, 'load', initialize);
