1d2c81e684ef718eac1fe1cc5f4c94a9eb85ef9c
Pablo <cotorrss@gmail.com>
2018-03-26 18:34:45 +0200

    Realizados test de la aplicacion
    Solo falta el caso de cambiar de estado

---
+++ src/test/java/com/e1b/pageobjects/PO_HomeView.java
@@ -0,0 +1,30 @@
+package com.e1b.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+
+import com.e1b.util.SeleniumUtils;
+
+public class PO_HomeView extends PO_NavView {
+
+
+	static public void checkWelcome(WebDriver driver, int language) {
+		// Esperamos a que se cargue el saludo de bienvenida en Español
+		SeleniumUtils.EsperaCargaPagina(driver, "text", p.getString("welcome.message", language),
+				getTimeout());
+	}
+
+	static public void checkChangeIdiom(WebDriver driver, String textIdiom1, String textIdiom2, int locale1,
+			int locale2) {
+		// Esperamos a que se cargue el saludo de bienvenida en Español
+		PO_HomeView.checkWelcome(driver, locale1);
+		// Cambiamos a segundo idioma
+		PO_HomeView.changeIdiom(driver, textIdiom2);
+		// COmprobamos que el texto de bienvenida haya cambiado a segundo idioma
+		PO_HomeView.checkWelcome(driver, locale2);
+		// Volvemos a Español.
+		PO_HomeView.changeIdiom(driver, textIdiom1);
+		// Esperamos a que se cargue el saludo de bienvenida en Español
+		PO_HomeView.checkWelcome(driver, locale1);
+	}
+
+}
