2cb376f9a9b78567c70f500ea253988fefcfd2b6
Pablo <cotorrss@gmail.com>
2018-05-01 20:19:20 +0200

    Añadidas mas pruebas de carga

---
+++ gatling_tests/com/e1b/ShowInciSimulation.scala
@@ -0,0 +1,51 @@
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class ShowInciSimulation extends Simulation {
+
+	val httpProtocol = http
+		.baseURL("http://localhost:8092")
+		.inferHtmlResources()
+		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("es-AR,es;q=0.8,en-US;q=0.5,en;q=0.3")
+		.userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:46.0) Gecko/20100101 Firefox/46.0")
+
+	val headers_1 = Map("Accept" -> "text/css,*/*;q=0.1")
+
+
+
+	val scn = scenario("ShowInciSimulation")
+		.exec(http("request_0")
+			.post("/login")
+			.formParam("username", "entidad2")
+			.formParam("password", "123456")
+			.formParam("${_csrf.parameterName}", "${_csrf.token}")
+			.resources(http("request_1")
+			.get("/css/custom.css")
+			.headers(headers_1),
+            http("request_2")
+			.get("/favicon.ico")))
+		.pause(1)
+		.exec(http("request_3")
+			.get("/incidencias/list")
+			.resources(http("request_4")
+			.get("/css/custom.css")
+			.headers(headers_1),
+            http("request_5")
+			.get("/favicon.ico")))
+		.pause(2)
+		.exec(http("request_6")
+			.get("/logout")
+			.resources(http("request_7")
+			.get("/css/custom.css")
+			.headers(headers_1),
+            http("request_8")
+			.get("/favicon.ico")))
+
+	setUp(scn.inject(rampUsers(10) over(5 seconds))).protocols(httpProtocol)
+}
