a2d7f4211bc7af8220e04d0af99d0c8e65b9aeb3
Pablo <cotorrss@gmail.com>
2018-05-02 12:13:16 +0200

    Prueba de carga para incidencias en tiempo real

---
+++ gatling_tests/com/e1b/RealTimeRecorded.scala
@@ -0,0 +1,53 @@
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class RealTimeRecorded extends Simulation {
+
+	val httpProtocol = http
+		.baseURL("http://localhost:8092")
+		.inferHtmlResources()
+		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("es-AR,es;q=0.8,en-US;q=0.5,en;q=0.3")
+		.userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:46.0) Gecko/20100101 Firefox/46.0")
+
+	val headers_1 = Map("Accept" -> "text/css,*/*;q=0.1")
+
+
+
+	val scn = scenario("RealTimeRecorded")
+		.exec(http("request_0")
+			.get("/login")
+			.resources(http("request_1")
+			.get("/css/custom.css")
+			.headers(headers_1),
+            http("request_2")
+			.get("/favicon.ico")))
+		.pause(5)
+		.exec(http("request_3")
+			.post("/login")
+			.formParam("username", "entidad2")
+			.formParam("password", "123456")
+			.formParam("${_csrf.parameterName}", "${_csrf.token}")
+			.resources(http("request_4")
+			.get("/css/custom.css")
+			.headers(headers_1),
+            http("request_5")
+			.get("/favicon.ico")))
+		.pause(3)
+		.exec(http("request_6")
+			.get("/incidencias/kafka-messages")
+			.resources(http("request_7")
+			.get("/logout"),
+            http("request_8")
+			.get("/css/custom.css")
+			.headers(headers_1),
+            http("request_9")
+			.get("/favicon.ico")))
+
+	setUp(scn.inject(rampUsers(10) over(50 seconds))).protocols(httpProtocol)
+}
