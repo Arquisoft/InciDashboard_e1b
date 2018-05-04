5a96c6a6f453895d38473b95db6f4c304e7f6b6e
RodriguezArtimeSergio <32743170+RodriguezArtimeSergio@users.noreply.github.com>
2018-05-04 19:20:05 +0200

    Retoques Operario

--- src/test/java/com/e1b/junit/TestOperario.java
+++ src/test/java/com/e1b/junit/TestOperario.java
@@ -60,19 +60,19 @@ public class TestOperario {
 		assertEquals(operario2.getPassword(), "4321");
 	}
 
-	@Test
-	public void testGetIncidencias() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetIncidencias() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void testGetIncidencias() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSetIncidencias() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testToString() {
+//		fail("Not yet implemented");
+//	}
 
 }
